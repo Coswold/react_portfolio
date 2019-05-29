e483824973db0ce69832d12fcf2ac72b4221ec7e
Connor Oswold <coswoldo@gmail.com>
2019-05-28 21:51:24 -0700

    trying to implement timeline

--- src/App.js
+++ src/App.js
@@ -2,9 +2,6 @@ import React, { Component } from 'react';
 import './App.css';
 
 import Navbar from './components/navbar'
-import Main from './components/main'
-import Projects from './components/projects'
-import Contact from './components/contact'
 
 class App extends Component {
     render() {
--- src/components/main.js
+++ src/components/main.js
@@ -1,7 +1,38 @@
 import React, { Component } from "react";
+import Timeline from './timeline';
 
 class Main extends Component {
-  render() {
+    state = {
+        timelineData: [
+          {
+            text: 'Wrote my first blog post ever on Medium',
+            date: 'March 03 2017',
+            category: {
+                tag: 'medium',
+                color: '#018f69'
+            },
+            link: {
+                url:
+                    'kotharidigital.com',
+                text: 'Read more'
+            }
+          },
+          {
+            text: 'This is the second timeline item',
+            date: 'September 22 2017',
+            category: {
+                tag: 'tag-two',
+                color: '#018f69'
+            },
+            link: {
+                url:
+                    'https://medium.com/@popflorin1705/javascript-coding-challenge-1-6d9c712963d2',
+                text: 'Read more'
+            }
+        }
+    ]
+}
+render() {
     return (
       <div>
         <h2>HELLO</h2>
@@ -9,6 +40,7 @@ class Main extends Component {
             computer science, and helping people find their passion.</p>
 
         <p>If you'd like to learn more, have a look around.</p>
+        <Timeline data={this.state.timelineData} />
       </div>
     );
   }
