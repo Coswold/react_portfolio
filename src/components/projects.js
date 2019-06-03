e60b12ba3eb67692fac1c4f5ecbefadf4d84645f
Connor Oswold <coswoldo@gmail.com>
2019-06-03 16:45:10 -0700

    finalized styling and projects

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,6 +1,10 @@
 import React, { Component } from "react";
 import '../cards.css';
-import img from "../logo8.png";
+import dbapi from "../DBAPI.png";
+import calc from "../calc.png";
+import stw from "../stw.png";
+import lib from "../lib.png";
+import ldg from "../loading.jpg";
 
 class Projects extends Component {
   render() {
@@ -8,7 +12,7 @@ class Projects extends Component {
       <div>
 
         <div class="topic bgb padding">
-            <h2>PROJECTS (UNDER CONSTRUCTION)</h2>
+            <h2>PROJECTS</h2>
             <h3>Machine Learning, Web, and Native Apps</h3>
             <p>Here are some of my favorite projects. Many are complete, but
             some are still in progress. All are open source, so check them out
@@ -23,14 +27,13 @@ class Projects extends Component {
             <div class="row">
               <div class="column">
                 <div class="card">
-                  <img src={img} />
-                  <h3>Card 1</h3>
-                  <p>Some text</p>
-                  <p>Some text</p>
-                  <a href="https://www.w3schools.com" target="_blank">
-                    <button class="button left">Deployment</button>
+                  <img src={ldg} />
+                  <h3>Sentence Generator</h3>
+                  <p>Python, Flask, Heroku, Bootstrap</p>
+                  <a href="https://github.com/Coswold/tweet_generator" target="_blank">
+                    <button class="button left">Visit</button>
                   </a>
-                  <a href="https://www.w3schools.com" target="_blank">
+                  <a href="https://github.com/Coswold/tweet_generator" target="_blank">
                     <button class="button right">Code</button>
                   </a>
                 </div>
@@ -38,10 +41,15 @@ class Projects extends Component {
 
               <div class="column">
                 <div class="card">
-                  <img src={img} />
-                  <h3>Card 1</h3>
-                  <p>Some text</p>
-                  <p>Some text</p>
+                  <img src={ldg} />
+                  <h3>In Progress</h3>
+                  <p>Python</p>
+                  <a href="https://www.github.com/coswold" target="_blank">
+                    <button class="button left">Visit</button>
+                  </a>
+                  <a href="https://www.github.com/coswold" target="_blank">
+                    <button class="button right">Code</button>
+                  </a>
                 </div>
               </div>
             </div>
@@ -53,19 +61,30 @@ class Projects extends Component {
             <div class="row">
               <div class="column">
                 <div class="card">
-                  <img src={img} />
-                  <h3>Card 1</h3>
-                  <p>Some text</p>
-                  <p>Some text</p>
+                  <img src={dbapi} />
+                  <h3>Dragon Ball API</h3>
+                  <p>RESTful practices, Node.js, MongoDB, Heroku, Bootstrap</p>
+                  <a href="https://dragon-ball-api.herokuapp.com/" target="_blank">
+                    <button class="button left">Visit</button>
+                  </a>
+                  <a href="https://github.com/Coswold/Dragon_Ball_API" target="_blank">
+                    <button class="button right">Code</button>
+                  </a>
                 </div>
               </div>
 
               <div class="column">
                 <div class="card">
-                  <img src={img} />
-                  <h3>Card 1</h3>
-                  <p>Some text</p>
-                  <p>Some text</p>
+                  <img src={calc} />
+                  <h3>Calcmon</h3>
+                  <p>Node.js, Express, Express-Handlebars, MongoDB, Socket.io,
+                  Heroku</p>
+                  <a href="https://calcumongame.herokuapp.com/" target="_blank">
+                    <button class="button left">Visit</button>
+                  </a>
+                  <a href="https://github.com/Coswold/Calcumon" target="_blank">
+                    <button class="button right">Code</button>
+                  </a>
                 </div>
               </div>
             </div>
@@ -77,19 +96,29 @@ class Projects extends Component {
             <div class="row">
               <div class="column">
                 <div class="card">
-                  <img src={img} />
-                  <h3>Card 1</h3>
-                  <p>Some text</p>
-                  <p>Some text</p>
+                  <img src={lib} />
+                  <h3>RoboLib</h3>
+                  <p>JavaScript, Electron, HTML/CSS</p>
+                  <a href="https://github.com/Coswold/electron_app" target="_blank">
+                    <button class="button left">Download</button>
+                  </a>
+                  <a href="https://github.com/Coswold/electron_app" target="_blank">
+                    <button class="button right">Code</button>
+                  </a>
                 </div>
               </div>
 
               <div class="column">
                 <div class="card">
-                  <img src={img} />
-                  <h3>Card 1</h3>
-                  <p>Some text</p>
-                  <p>Some text</p>
+                  <img src={stw} />
+                  <h3>Slay the Wompum</h3>
+                  <p>JavaScript</p>
+                  <a href="https://github.com/Coswold/slay_the_wompum" target="_blank">
+                    <button class="button left">Download</button>
+                  </a>
+                  <a href="https://github.com/Coswold/slay_the_wompum" target="_blank">
+                    <button class="button right">Code</button>
+                  </a>
                 </div>
               </div>
             </div>
